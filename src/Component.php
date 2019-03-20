969dd58db4570824af722c9ca0b0a82add92f6c0
Zdenek Jirku <zdenek.jirku@usetreno.cz>
2019-03-20 15:33:22 +0100

    init

--- src/Component.php
+++ src/Component.php
@@ -10,7 +10,7 @@ class Component extends BaseComponent
 {
     public function run(): void
     {
-        // @TODO implement
+        echo "ja ";
     }
 
     protected function getConfigClass(): string
